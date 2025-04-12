1f5b9ef1e29bc4cb2a4af9fbbfc3f59301f868ea
Gabodev <51827577+Gguardiola@users.noreply.github.com>
2025-04-12 17:08:31 +0200

    feat: Added new interactions and EventOnly triggers (#39)

---
+++ Assets/Scripts/Interactables/AnimateCube.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class AnimateCube : Interactable
+{
+    Animator animator;
+
+    private string startPrompt;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        startPrompt = promptMessage;
+    }
+
+    private void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Default"))
+        {
+            promptMessage = startPrompt;
+        }
+        else
+        {
+            promptMessage = "Stop animating";
+        }
+    }
+
+    protected override void Interact()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Spinning"))
+        {
+         animator.Play("Default");   
+        }
+        else
+        {
+            animator.Play("Spinning");
+        }
+    }
+}
