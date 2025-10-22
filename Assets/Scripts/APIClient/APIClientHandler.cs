dfcf1588c140f3bb4ba8607829198ec273c0b1dc
Gguardiola <gabriel.guardiola1999@gmail.com>
2025-10-22 23:23:05 +0200

    refactor: API Client handler post combatlog fix

--- Assets/Scripts/APIClient/APIClientHandler.cs
+++ Assets/Scripts/APIClient/APIClientHandler.cs
@@ -47,7 +47,7 @@ public class APIClientHandler : MonoBehaviour
         {
                 if (apiclient != null && apiclient.IsLoggedIn && !loadSent)
                 {
-                        StartCoroutine(apiclient.PostCombatLog(jsonPayload));
+                        //StartCoroutine(apiclient.PostCombatLog(jsonPayload));
                         loadSent = true;
                 }
         }
